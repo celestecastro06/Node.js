3a43def726defdfbb0fe9811060b817a80ffce92
celestecastro06 <dania.ccc6@gmail.com>
2025-03-08 00:56:07 -0600

    Modifique index.js

--- index.js
+++ index.js
@@ -8,16 +8,37 @@ app.get("/",(req,res, next) => {
     res.send("Bienvenido al Pokedex");
 });
 
-app.get('/pokemon', (req, res, next) => {
+app.get('/pokemon/all', (req, res, next) => {
     res.status(200);
     res.send(pokemon);
 });
 
-app.get('/pokemon/:id', (req, res, next) => {
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const id = req.params.id - 1;
+   if(id >= 0 && id <= 150){
     res.status(200);
     res.send(pokemon[req.params.id - 1]);
-})
+	}
+	else {
+	  res.status(404);
+	  res.send("Pokemon no encontrado.");
+	}
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+	const name = req.params.name;
+	for(i = 0; i < pokemon.length; i++) {
+		if(pokrmon[i].name == name){
+		res.status(200);
+		res.send(pokemon);
+		}
+	}
+});
 
 app.listen(process.env.PORT || port,()=>{
     console.log("Corriendo en el puerto ", port)
-})
+});
+
+
+
+

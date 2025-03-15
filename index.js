5ec3d86377afb73621c84f5c97ae7dba8dd3981d
celestecastro06 <dania.ccc6@gmail.com>
2025-03-15 01:55:20 -0600

    NodeJS 04 - POST, Body Parser y Postman

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,36 +12,46 @@ PUT - modificar recursos
 DELETE - borrar un recurso
 */
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/",(req,res, next) => {
     return res.status(200).send("Bienvenido al Pokedex");
-});
+}); 
 
-app.get('/pokemon/all', (req, res, next) => {
-    return res.statu(200).send(pokemon);
+app.post("/pokemon", (req, res, next) => {
+	return res.status(200).send(req.body);
+})
+
+app.get('/pokemon', (req, res, next) => {
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id - 1;
-   (id >= 0 && id <= 150) ?
-     res.status(200).send(pokemon[req.params.id - 1]) :
-	 res.status(404).send("Pokemon no encontrado.");
+
+   if (id >= 0 && id <= 150) {
+     res.status(200).send(pokemon[req.params.id - 1]);
+   }
+   return res.status(404).send("Pokemon no encontrado.");
 });
 
 app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
 	const name = req.params.name;
 	
 	const pk = pokemon.filter((p) => {
-		return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+		return (p.name.toUpperCase() == name.toUpperCase()) && p;
 		});
 
-	(pk.length > 0) ? 
-		res.status(200).send(pk) :
-		res.status(404).send("Pokemon no encontrado");
+	if (pk.length > 0) {
+		return res.status(200).send(pk);
+	}
+	return res.status(404).send("Pokemon no encontrado");
 
 });
 
-app.listen(process.env.PORT || port,()=>{
-    console.log("Corriendo en el puerto ", port)
+app.listen(process.env.PORT || port,() => {
+    console.log("Corriendo en el puerto... ", port)
 });
 
 

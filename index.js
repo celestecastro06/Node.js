b1012602e109cc5a31cc0b4aabc48c8bb90e81e1
celestecastro06 <dania.ccc6@gmail.com>
2025-03-15 02:32:12 -0600

    NodeJS 05 - Router y Morgan

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const port = 3000;
-const { pokemon } = require('./pokedex.json');
+const pokemon = require('./routes/pokemon');
 
 /* 
 GET - obtener recursos
@@ -12,43 +12,16 @@ PUT - modificar recursos
 DELETE - borrar un recurso
 */
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/",(req,res, next) => {
     return res.status(200).send("Bienvenido al Pokedex");
 }); 
 
-app.post("/pokemon", (req, res, next) => {
-	return res.status(200).send(req.body);
-})
-
-app.get('/pokemon', (req, res, next) => {
-    return res.status(200).send(pokemon);
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-    const id = req.params.id - 1;
-
-   if (id >= 0 && id <= 150) {
-     res.status(200).send(pokemon[req.params.id - 1]);
-   }
-   return res.status(404).send("Pokemon no encontrado.");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-	const name = req.params.name;
-	
-	const pk = pokemon.filter((p) => {
-		return (p.name.toUpperCase() == name.toUpperCase()) && p;
-		});
-
-	if (pk.length > 0) {
-		return res.status(200).send(pk);
-	}
-	return res.status(404).send("Pokemon no encontrado");
-
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || port,() => {
     console.log("Corriendo en el puerto... ", port)

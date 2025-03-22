c7aaca61bea141ffe3f30bcaba54f87904826885
celestecastro06 <dania.ccc6@gmail.com>
2025-03-21 23:44:50 -0600

    NodeJS 07 - Cambiando body parser

--- index.js
+++ index.js
@@ -18,10 +18,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/",(req,res, next) => {
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({ code: 1, message: "Bienvenido al pokedex!!" });
 }); 
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: "URL no encontrada."});
+});
 
 app.listen(process.env.PORT || port,() => {
     console.log("Corriendo en el puerto... ", port)
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,28 +8,25 @@ pokemon.post("/", (req, res, next) => {
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 });
 
-pokemon.get('/:id([0-9]{1,3})', (req, res, next) => {
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id - 1;
-   if (id >= 0 && id <= 150) {
-     res.status(200).send(pk[req.params.id - 1]);
+   if (id >= 1 && id <= 722) {
+	const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
+     res.status(200).json({code: 1, message: pokemon});
    }
-   return res.status(404).send("Pokemon no encontrado.");
+   return res.status(404).send({code: 1, message:"Pokemon no encontrado."});
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
 	const name = req.params.name;
-	
-	const pkmn = pk.filter((p) => {
-		return (p.name.toUpperCase() == name.toUpperCase()) && p;
-		});
-
+	const pkmn = await db.query("SELESCT * FROM pokemon WHERE pok_name='"+name+"';");
 	if (pkmn.length > 0) {
-		return res.status(200).send(pkmn);
+		res.status(200).json({code: 1, message: pokemon});
 	}
-	return res.status(404).send("Pokemon no encontrado");
+	return res.status(404).send({code: 1, message:"Pokemon no encontrado."});
 
 });
 
